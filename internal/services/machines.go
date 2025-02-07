1f61a42139c4fff614dc6312a111f6d582178012
Mitron57 <armenchik2006@gmail.com>
2025-02-07 23:09:48 +0400

    Rip the useless cache off

--- internal/services/machines.go
+++ internal/services/machines.go
@@ -5,17 +5,11 @@ import (
     "dockMon/internal/domain/interfaces/infrastructure"
     "dockMon/internal/domain/interfaces/services"
     "dockMon/internal/domain/models"
-    "maps"
-    "slices"
-    "sync"
     "time"
 )
 
 type MachinesService struct {
     repo   infrastructure.MachineRepository
-    mx     sync.Mutex
-    cache  map[string]*models.Machine
-    last   time.Time
     period time.Duration
 }
 
@@ -24,24 +18,9 @@ func NewMachinesService(repo infrastructure.MachineRepository, period time.Durat
 }
 
 func (m *MachinesService) Save(ctx context.Context, machine *models.Machine) error {
-    if time.Now().Before(m.last.Add(m.period)) {
-        m.mx.Lock()
-        m.cache[machine.IP] = machine
-        m.mx.Unlock()
-    }
     return m.repo.Put(ctx, machine)
 }
 
 func (m *MachinesService) Machines(ctx context.Context) ([]*models.Machine, error) {
-    if time.Now().After(m.last.Add(m.period)) {
-        m.last = time.Now()
-        cache, err := m.repo.All(ctx)
-        if err != nil {
-            return nil, err
-        }
-        m.mx.Lock()
-        defer m.mx.Unlock()
-        m.cache = cache
-    }
-    return slices.Collect(maps.Values(m.cache)), nil
+    return m.repo.All(ctx)
 }
